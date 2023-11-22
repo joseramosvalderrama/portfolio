27d9b1ab7557396a5286b00b76ff849386d9269b
joseramosvalderrama <jmramosvalderrama@gmail.com>
2023-11-22 20:30:18 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="flex flex-row">
+      <div className="basis-1/2">
+        <h1 className="text-3xl font-bold">José Manuel Ramos</h1>
+      </div>
+      <div>
+        <text>Right side</text>
+      </div>
+    </div>
+  );
+}
