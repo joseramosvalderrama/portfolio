b43b3a4e387b769b01f13c0a02dc8cf5f8e30e45
joseramosvalderrama <jmramosvalderrama@gmail.com>
2023-11-22 20:32:48 +0100

    using just tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="flex flex-row">
       <div className="basis-1/2">
-        <h1 className="text-3xl font-bold">José Manuel Ramos</h1>
+        <h1 className="text-3xl font-bold">José Manuel Ramos Valderrama</h1>
       </div>
       <div>
         <text>Right side</text>
