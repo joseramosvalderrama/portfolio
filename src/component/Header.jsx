eba3261586568bb9e8f2d2b6a690eebc92fbbd79
joseramosvalderrama <jmramosvalderrama@gmail.com>
2024-05-19 11:43:04 +0200

    Responsive design

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,9 +2,9 @@ import photo from "../assets/profile_photo.png";
 
 export default function Header() {
   return (
-    <div className="flex flex-row items-center bg-gray-800">
-      <div className="basis-1/5 flex justify-center">
-        <img src={photo} className="w-60"></img>
+    <div className="flex flex-row items-center bg-gray-800 min-h-[200px]">
+      <div className="flex justify-center mr-3">
+        <img src={photo} className="w-60 min-h-[100px]"></img>
       </div>
       <div className="">
         <h1 className="text-4xl font-extrabold text-gray-100">
