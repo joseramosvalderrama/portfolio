b254ce3d6b1d7a9f694808c884a035c34ce2f430
joseramosvalderrama <jmramosvalderrama@gmail.com>
2024-05-19 10:59:25 +0200

    Responsive portfolio

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -7,10 +7,10 @@ export default function Body() {
   return (
     <div className="bg-gray-50 mt-10">
       <div className="flex flex-row flex-wrap">
-        <div className="basis-1/2">
+        <div className="flex-grow basis-1/2 min-w-[600px]">
           <Section title="Work experience" cardList={workExperienceData} />
         </div>
-        <div className="basis-1/2">
+        <div className="flex-grow basis-1/2">
           <Section title="Education" cardList={educationData} />
         </div>
       </div>
